6e8bb9dd0d66bbccfa02673b092f9c5ff1bbdd00
Scott Martin <scott@scottmartin.systems>
2021-12-30 00:30:34 +0000

    [strip-url-params] Add URL parameter stripper

---
+++ strip-url-params/strip-url-params.js
@@ -0,0 +1,28 @@
+function clean(e) {
+	const result = document.getElementById('result')
+	const copyButton = document.getElementById('copyButton')
+	
+	document.getElementById('copyLabel').innerText = ''
+
+	const urlNoParams = /^(https?:\/\/.*?)\?/
+
+	let matches = urlNoParams.exec(e.target.value)
+	
+	result.innerText = matches ? matches[1] : e.target.value
+
+	copyButton.style.display = result.innerText.length
+		? 'inline'
+		: 'none'
+}
+
+function copyOutput() {
+	navigator.clipboard.writeText(
+		document.getElementById('result').innerText
+	)
+
+	document.getElementById('copyLabel').innerText = 'Copied.'
+} 
+
+document.getElementById('url').value = ''
+document.getElementById('url').addEventListener('input', clean)
+document.getElementById('copyButton').addEventListener('click', copyOutput)
