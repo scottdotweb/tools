ecc83ab589a503bd4374761cb52f04b6c6387a18
Scott Martin <scott@scottmartin.systems>
2022-03-24 00:00:47 +0000

    [pert] Extract titles and dates of unavailable videos from Wayback Machine captures 😎

--- pert/pert.mjs
+++ pert/pert.mjs
@@ -1,3 +1,5 @@
+import chalk from 'chalk'
+import * as cheerio from 'cheerio'
 import fetch from 'node-fetch'
 import fs from 'fs'
 
@@ -8,45 +10,61 @@ const PLAYLIST = process.argv.slice(2)[0] ? process.argv.slice(2)[0] : 'playlist
 	- Make this a web app
 	- Use Web Workers to fetch archive captures
 		https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
-	- Use Cheerio to get the video title from archive captures
-		https://www.npmjs.com/package/cheerio
 	- (Eventually) Use YouTube Data API to get JSON for playlists
 		https://developers.google.com/youtube/v3/docs
 */
 
 for (const video of JSON.parse(fs.readFileSync(PLAYLIST, 'utf8'))) {
-	const isDeleted = video.snippet.title === 'Deleted video'
-	const isPrivate = video.snippet.title === 'Private video'
-
-	const unavailable = (isDeleted || isPrivate)
-
-	let videoDate = 'unknown date'
-
-	if (!unavailable) {
-		const date = new Date(video.contentDetails.videoPublishedAt)
-
-		videoDate = new Intl.DateTimeFormat('en-GB').format(date)
-	}
-
 	const videoId = video.contentDetails.videoId
-
 	const videoUrl = `https://youtube.com/watch?v=${videoId}`
 
-	const currentUrl = unavailable
-		? await queryWaybackMachine(videoUrl)
-		: videoUrl
+	let videoTitle, rawDate, currentUrl
 
-	console.info(`${unavailable ? '❌ ' : ''}${video.snippet.title} (${videoDate})
-${currentUrl}
-`)
+	if (
+		video.snippet.title === 'Deleted video' ||
+		video.snippet.title === 'Private video'
+	) {
+		const archivedUrl = await queryWaybackMachine(videoUrl)
+
+		const html = await getSnapshot(archivedUrl)
+		const $ = cheerio.load(html)
+
+		if ($('#watch-headline-title'))
+			videoTitle = $('#watch-headline-title').text().trim()
+		else
+			videoTitle = $('.watch-title').text().trim()
+
+		videoTitle = '❌ ' + chalk.red(videoTitle)
+		rawDate = new Date($('.watch-video-date').text().trim())
+	} else {
+		currentUrl = videoUrl
+		videoTitle = video.snippet.title
+		rawDate = new Date(video.contentDetails.videoPublishedAt)
+	}
+
+	console.info(`${chalk.bold(videoTitle)} (${
+		new Intl.DateTimeFormat('en-GB').format(rawDate)
+	})\n${currentUrl}\n`)
 }
 
-async function queryWaybackMachine (videoUrl) {
-	return await fetch(`http://archive.org/wayback/available?url=${videoUrl}&timestamp=20050214`)
+function queryWaybackMachine (videoUrl) {
+	return fetch(`http://archive.org/wayback/available?url=${videoUrl}&timestamp=20050214`)
 		.then(response => response.json())
 		.then(data => {
 			const archivedUrl = data.archived_snapshots?.closest?.url
 
 			return archivedUrl ? archivedUrl : `${videoUrl} (not archived)`
+		})
+		.catch(error => {
+			console.error('Error:', error)
+		})
+}
+
+function getSnapshot (archivedUrl) {
+	return fetch(archivedUrl)
+		.then(response => response.text())
+		.then(data => data)
+		.catch(error => {
+			console.error('Error:', error)
 		})
 }
