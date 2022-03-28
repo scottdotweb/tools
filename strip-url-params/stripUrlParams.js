af536afe25a812666e534bd42225e8e079ec8b44
Scott Martin <scott@scottmartin.systems>
2022-03-28 01:05:34 +0100

    [strip-url-params] Make significantly less ugly

--- strip-url-params/stripUrlParams.js
+++ strip-url-params/stripUrlParams.js
@@ -1,10 +1,14 @@
 const e = el => document.getElementById(el)
+const urlBox = e('url')
 
+const sampleUrl = 'https://example.com/post/whatever?tracking-id=abcdef123456'
+
+// TODO: Handle bad input
 function clean (rawText) {
 	const resultBox = e('result')
 	const resultContainer = e('resultContainer')
 
-	e('copyLabel').innerText = ''
+	e('copyNotification').innerText = ''
 
 	const urlNoParams = /^.*?(https?:\/\/.*?)\?/
 
@@ -24,7 +28,7 @@ function clean (rawText) {
 function copyOutput () {
 	navigator.clipboard.writeText(e('result').innerText)
 
-	e('copyLabel').innerHTML = '<p>Copied!</p>'
+	e('copyNotification').innerHTML = 'Copied!'
 }
 
 function getParam (param) {
@@ -37,16 +41,19 @@ window.addEventListener('DOMContentLoaded', () => {
 	const sharedUrl = getParam('url') || getParam('link') || getParam('text')
 
 	if (sharedUrl) {
-		e('url').value = sharedUrl
+		urlBox.value = sharedUrl
 		clean(sharedUrl)
 	}
 })
 
 // --------------------------------------------------------------------------
 
-e('url').value = ''
-e('url').addEventListener('input', e => clean(e.target.value))
-e('copyButton').addEventListener('click', copyOutput)
+urlBox.innerText = sampleUrl
+urlBox.addEventListener('click', e => {
+	if (e.target.innerHTML === sampleUrl) e.target.innerHTML = ''
+})
+urlBox.addEventListener('input', e => clean(e.target.value))
+e('result').addEventListener('click', copyOutput)
 
 if ('serviceWorker' in navigator)
 	navigator.serviceWorker.register('serviceWorker.js', { scope: '.' })
