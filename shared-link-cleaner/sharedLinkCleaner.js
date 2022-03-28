741668adcd6fb100dbf93866fece2474b7004ba5
Scott Martin <scott@scottmartin.systems>
2022-03-28 23:51:04 +0100

    [shared-link-cleaner] Set/clear input placeholder as necessary; adjust layout to avoid footer movement; some code tidying

--- shared-link-cleaner/sharedLinkCleaner.js
+++ shared-link-cleaner/sharedLinkCleaner.js
@@ -1,15 +1,47 @@
-const e = el => document.getElementById(el)
-const urlBox = e('url')
-const footer = e('footer')
+const q = s => document.querySelector(s)
 
-const sampleUrl = 'https://example.com/post/whatever?tracking-id=abcdef123456'
+const urlBox = q('#url')
+const resultContainerInner = q('#resultContainerInner')
+const resultBox = q('#result')
+const copyNotification = q('#copyNotification')
+
+const SAMPLE_URL = 'https://example.com/post/whatever?tracking-id=abcdef123456'
+
+function prepUrlBoxIfNecessary () {
+	if (urlBox.value === SAMPLE_URL) prepUrlBox()
+}
+
+function prepUrlBox () {
+	urlBox.value = ''
+	urlBox.style.color = '#000'
+}
+
+function resetUrlBox () {
+	urlBox.style.color = '#696969'
+	urlBox.value = SAMPLE_URL
+}
+
+function handleInput () {
+	if (urlBox.value === '') {
+		hideResultBox()
+		return
+	}
+
+	clean(urlBox.value)
+}
+
+function hideResultBox () {
+	resultContainerInner.style.display = 'none'
+}
 
 // TODO: Handle bad input
 function clean (rawText) {
-	const resultBox = e('result')
-	const resultContainer = e('resultContainer')
+	if (urlBox.value === SAMPLE_URL) {
+		hideResultBox()
+		return
+	}
 
-	e('copyNotification').innerText = ''
+	copyNotification.innerText = ''
 
 	const urlNoParams = /^.*?(https?:\/\/.*?)\?/
 
@@ -17,45 +49,45 @@ function clean (rawText) {
 
 	let result = matches ? matches[1] : rawText
 
+	// Special cases
 	if (result.match(/\.amazon./)) result = result.replace(/\/ref=.*?$/, '')
 
 	resultBox.innerText = result
 
-	resultContainer.style.display = resultBox.innerText.length
-		? 'block'
-		: 'none'
+	resultContainerInner.style.display = 'block'
 }
 
 function copyOutput () {
-	navigator.clipboard.writeText(e('result').innerText)
+	navigator.clipboard.writeText(resultBox.innerText)
 
-	footer.style.marginTop = '2.5em'
-	e('copyNotification').innerHTML = 'Copied!'
+	copyNotification.innerHTML = 'Copied!'
 }
-
-function getParam (param) {
-	const parsedUrl = new URL(window.location)
-
-	return parsedUrl.searchParams.get(param)
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-	const sharedUrl = getParam('url') || getParam('link') || getParam('text')
-
-	if (sharedUrl) {
-		urlBox.value = sharedUrl
-		clean(sharedUrl)
-	}
-})
 
 // --------------------------------------------------------------------------
 
-urlBox.innerText = sampleUrl
-urlBox.addEventListener('click', e => {
-	if (e.target.innerHTML === sampleUrl) e.target.innerHTML = ''
+urlBox.innerText = SAMPLE_URL
+
+q('body').addEventListener('click', event => {
+	if (!event.target.closest('#url') && urlBox.value === '') resetUrlBox()
 })
-urlBox.addEventListener('input', e => clean(e.target.value))
-e('result').addEventListener('click', copyOutput)
+
+urlBox.addEventListener('click', prepUrlBoxIfNecessary)
+urlBox.addEventListener('beforeinput', prepUrlBoxIfNecessary)
+urlBox.addEventListener('input', handleInput)
+
+resultBox.addEventListener('click', copyOutput)
+q('#copyInfo').addEventListener('click', copyOutput)
+
+window.addEventListener('DOMContentLoaded', () => {
+	const getParam = param => new URL(window.location).searchParams.get(param)
+
+	const sharedUrl = getParam('url') || getParam('link') || getParam('text')
+
+	if (!sharedUrl) return
+
+	urlBox.value = sharedUrl
+	clean(sharedUrl)
+})
 
 if ('serviceWorker' in navigator)
 	navigator.serviceWorker.register('serviceWorker.js', { scope: '.' })
