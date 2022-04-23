5e46d5ca8237f4c7c7afeabe7950622744ac38cf
Scott Martin <scott@scottmartin.systems>
2022-04-23 17:16:33 +0100

    [bookmarklets] Add Metacritic yearly top hundred

---
+++ bookmarklets/js/metacriticYearlyTopHundred.js
@@ -0,0 +1,84 @@
+const junk = [
+	'meta', 'link', 'script', 'style', 'iframe',
+]
+
+for (const junkSelector of junk)
+	document.querySelectorAll(junkSelector).forEach(
+		junkItem => junkItem.remove()
+	)
+
+document.body.removeAttribute('class')
+
+const style = document.createElement('link')
+style.setAttribute('rel', 'stylesheet')
+style.setAttribute('href', 'https://scottdotjs.github.io/tools/bookmarklets/styles/metacriticYearlyTopHundred.css')
+
+document.head.appendChild(style)
+
+const year = document.querySelector('.year button').innerText
+
+const pageTitle = `Metacritic users' top 100 films for ${year}`
+
+const h1 = document.createElement('h1')
+h1.innerText = pageTitle
+document.head.querySelector('title').innerText = pageTitle
+
+const table = document.createElement('table')
+
+let count = 0
+
+for (const input of document.querySelectorAll('.clamp-list tr:not([class=spacer])')) {
+	count++
+
+	const title = input.querySelector('a.title').innerText
+	const score = input.querySelector('div.metascore_w').innerText
+	const imgSrc = input.querySelector('.clamp-image-wrap img').src
+
+	const tr = document.createElement('tr')
+
+	const seenCell = document.createElement('td')
+	const seenBox = document.createElement('div')
+	seenBox.setAttribute('class', 'seen')
+	seenCell.appendChild(seenBox)
+
+	const imageCell = document.createElement('td')
+	const image = document.createElement('img')
+	image.setAttribute('src', imgSrc)
+	imageCell.appendChild(image)
+
+	const countCell = document.createElement('td')
+	countCell.setAttribute('class', 'count')
+	countCell.innerText = count
+
+	const titleCell = document.createElement('td')
+	titleCell.setAttribute('class', 'title')
+	titleCell.innerText = title
+
+	const scoreCell = document.createElement('td')
+	const scoreBox = document.createElement('div')
+	scoreBox.innerText = score
+
+	scoreBox.classList.add('score')
+	const scoreGroup = score >= 70
+		? 'high'
+		: score >= 50
+			? 'medium'
+			: 'low'
+	scoreBox.classList.add(scoreGroup)
+	scoreCell.appendChild(scoreBox)
+
+	tr.appendChild(imageCell)
+	tr.appendChild(countCell)
+	tr.appendChild(titleCell)
+	tr.appendChild(scoreCell)
+	tr.appendChild(seenCell)
+
+	table.appendChild(tr)
+}
+
+document.body.innerHTML = ''
+
+const main = document.createElement('main')
+main.appendChild(h1)
+main.appendChild(table)
+document.body.appendChild(main)
